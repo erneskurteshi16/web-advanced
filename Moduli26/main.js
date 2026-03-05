1604b150c95b9792ece5d96ca9b7ea371919a0c0
erneskurteshi16 <erneskurteshi@gmail.com>
2026-03-05 17:41:21 +0100

    Javascript

--- Moduli26/main.js
+++ Moduli26/main.js
@@ -1,1 +1,74 @@
-console.log('Helloo')
+console.log('Helloo');
+
+var x="10";
+
+ var value="Digital School";
+
+var y="30";
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+var a=99;
+
+a++;
+
+console.log(a);
+
+var b=201;
+
+b--;
+
+console.log(b);
+
+var c="500";
+
+c+=5;
+
+console.log(c);
+
+var d="55";
+
+d-=10;
+
+console.log(d);
+
+var e="300";
+
+e*=5;
+
+console.log(e);
+
+var f="400";
+
+f/=10;
+
+console.log(f);
+
+// operatort e krahasimit
+
+var v="100";
+
+var w="50";
+
+console.log(v == w);
+
+console.log(v != w);
+
+console.log(v > w);
+
+console.log(v < w);
+
+var emri="Ernes";
+var mbiemri="Kurteshi";
+
+var fullName=emri +" "+mbiemri;
+
+console.log(fullName);
+
+console.log(`Hello from ${emri}`);
