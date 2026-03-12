8661f6bb9e0a58b787959b87bcdcccb2e8a6114c
erneskurteshi16 <erneskurteshi@gmail.com>
2026-03-12 17:47:48 +0100

    Moduli27

---
+++ Moduli27/main.js
@@ -0,0 +1,58 @@
+// function displayAlert(){
+//     alert("This text is inside the function!");
+// }
+
+
+// displayAlert();
+
+
+
+// function mbledhja(numri1,numri2){
+//     return numri1+numri2;
+// }
+
+
+// document.write(mbledhja(5,8));
+
+// function toCelsius(f){
+//     return 5/9*(f-32);
+// }
+
+// console.log(" 5 faronhite is equal to "+ toCelsius(5))
+
+// var arrayFunction = () => alert("Helloo");
+
+// arrayFunction();
+
+// function dsFunction(){
+//      var localvar="Digital School"
+//      alert(localvar);
+// }
+
+// dsFunction();
+
+// function toSeconds(m){
+//     return m*60;
+// }
+
+// console.log(toSeconds(5));
+
+// function toPerimeter(a,b,c){
+//     return a+b+c;
+// }
+
+// console.log(toPerimeter(3,2,5));
+
+var Car = {name:"BMW", color="red", viti=2008, vin="bbb8525", licencePlate:"01-255-NV"};
+
+alert(car.licencePlate);
+
+function Computer(name, CPU, RAM, GPU){
+this.name=name;
+this.CPU=CPU;
+this.RAM=RAM;
+this.GPU=GPU;
+}
+
+var c1=new Computer("Macbook","8-core","8GB","5600M GPU");
+
