bfd93b403bcacc3e8be16138ca53ec392611eace
erneskurteshi16 <erneskurteshi@gmail.com>
2026-03-24 17:49:53 +0100

    moduli29

--- Moduli28/main.js
+++ Moduli28/main.js
@@ -1,8 +1,8 @@
-// var button=document.getElementById(btn1);
+var button=document.getElementById('btn1');
 
-// button1.onclick = function(){
-//     alert('Hello')
-// }
+button.onclick = function(){
+    alert('Hello')
+}
 
 var button2 =document.querySelector|('button');
   
@@ -10,47 +10,47 @@ var button2 =document.querySelector|('button');
     alert('other button was clicked')
     }
 
-    var button2 =document.getElementById('btn2')
+var button2 =document.getElementById('btn2')
 
     button2.onmouseover=function(){
         alert('Mouse is over');
     }
 
-     var button3 =document.getElementById('btn3')
+var button3 =document.getElementById('btn3')
 
      button3.onmouseleave=function(){
         alert('Mouse left');
     }
 
-    var button4 =document.getElementById('btn4')
+var button4 =document.getElementById('btn4')
 
     button4.onmousewheel=function(){
         alert('Mouse wheel');
     }
 
-    var v_button=document.getElementById('btn6')
+    // var v_button=document.getElementById('btn6')
 
-    var v_text=document.getElementById('text1')
-    v_button.onclick=function(){
-    v_text.style.color='red'
-    }
+    // var v_text=document.getElementById('text1')
+    // v_button.onclick=function(){
+    // v_text.style.color='red';
+    // }
 
     var button1 = document.getElementById('btn11')
     var teksti = document.getElementById('txt1')  
 
-    var button2 = document.getElementById('btn22')
+    var button22 = document.getElementById('btn22')
     var teksti1 = document.getElementById('txt2')  
 
     button1.onclick = function(){
-        teksti.style.color='red'
-        teksti.style.backgroundColor='lightgrey'
-        teksti.style.fontSize = '100px'
-        teksti.style.padding = '20px'
-        teksti.style.textAlign = 'center'
+        teksti.style.color="red";
+        teksti.style.backgroundColor="lightgrey";
+        teksti.style.fontSize = "100px";
+        teksti.style.padding = "20px";
+        teksti.style.textAlign = "center";
     }
 
-    button2.onclick=function(){
-        teksti1.style.cssText="color:red; background-color:green; text-Align:center; font-Size:25px; font-family:fantasy; "
+    button22.onclick=function(){
+        teksti1.style.cssText="color:red; background-color:green; text-Align:center; font-Size:25px; font-family:fantasy; ";
     }
 
     var ifundit = document.getElementById('ifundit')
@@ -59,3 +59,42 @@ var button2 =document.querySelector|('button');
     ifundit.onclick=function(){
         pgg.setAttribute('class','paragrafi')
     }
+
+    var btn_circle = document.getElementById("btnCircle")
+    var btn_rect = document.getElementById('btnRect')
+    var btn_triangle = document.getElementById("btnTriangle")
+
+    var circle = document.getElementById("circle")
+    var rect = document.getElementById("rect")
+    var triangle = document.getElementById("triangle")
+
+    btn_circle.onclick=function()
+    {
+        circle.setAttribute("class","shape_circle");
+    }
+
+    btn_rect.onclick=function()
+    {
+        rect.setAttribute("class","shep_rect");
+    }
+
+    btn_triangle.onclick=function()
+    {
+        triangle.setAttribute("class","shape_triangle");
+    }
+
+    circle.onclick=function()
+    {
+        circle.style.backgroundColor=("class", "hide");
+    }   
+
+    rect.onclick=function()
+    {
+        rect.style.backgroundColor=("class", "hide");
+    }
+
+    triangle.onclick=function()
+    {
+        triangle.style.borderBottomColor=("class", "hide");
+    }   
+
---
+++ Moduli29/main.js
@@ -0,0 +1,31 @@
+var programmingLanguages = ["JavaScript","PHP","Python"];
+
+console.log(programmingLanguages);
+
+var numrat = [1,2,3,4,5,6,7,8,9,10];
+
+console.log(numrat);
+
+console.log(programmingLanguages[1]);
+
+// array function
+
+programmingLanguages.push("Java");
+
+numrat.push(11);
+
+document.write(programmingLanguages);
+
+document.write(numrat); 
+
+// pop deletes the last element
+
+numrat.pop();
+
+console.log(numrat);
+
+// unshift - adds an element at the beginning of the list
+
+programmingLanguages.unshift("C#");
+
+console.log(programmingLanguages);
