dcf1112cd3efb90cab3cfd4e3bd270f7710a4a92
erneskurteshi16 <erneskurteshi@gmail.com>
2026-03-26 17:45:36 +0100

    Moduli30

---
+++ Moduli30/main.js
@@ -0,0 +1,19 @@
+// var mosha = 10;
+
+// if (mosha <8){
+//     document.write("Your child is to young to learn programming.");
+// }else{
+//     document.write("We will call you soon to let you know about the next steps.");
+// }
+
+var age = 6;
+
+if(age <8){
+    document.write("Your child is to young to learn programming.");
+}else if(age >=18){
+    document.write("Your child is old enough to learn programming.");
+}
+
+else{
+    document.write("We will call you soon to let you know about the next steps.");
+}
