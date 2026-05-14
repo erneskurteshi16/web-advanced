9faff1e3b2cee4ab5a921fe6ac03dc47567c6ed8
erneskurteshi16 <erneskurteshi@gmail.com>
2026-05-14 17:47:32 +0200

    Moduli37

---
+++ Moduli37/js/main.js
@@ -0,0 +1,13 @@
+$("#sort").sortable()
+
+$(function(){
+    $("#accordion").accordion();
+});
+
+// $("#tabs").tabs();
+
+$(function(){
+    $("tabs").accordion({
+        collapsible:true
+    })
+})
